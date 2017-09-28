67db9c132dc729eafc6fe5a160d73787df33565f
Cristian Colorado <cristian.colorado@hbr.org>
2017-09-28 09:18:31 -0500

    Fix: Cleaning meal list after active user is changed

--- services/cdn/src/js/components/user/reducers/index.js
+++ services/cdn/src/js/components/user/reducers/index.js
@@ -1,9 +1,11 @@
 import { FETCH_MEALS, UPDATE_MEAL, POST_MEAL, DELETE_MEAL, SELECT_MEAL,
   UPDATE_CONSUMED_DATE, UPDATE_CONSUMED_DATE_TIME } from '../actions';
 import { UNAUTH_USER } from 'authentication/actions';
+import { ACTIVE_PROFILE } from 'screen/actions';
 
 export function mealListReducer(state = null, action) {
   switch (action.type) {
+    case ACTIVE_PROFILE:
     case UNAUTH_USER:
       return null;
     case FETCH_MEALS:
